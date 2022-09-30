d38f0b726c9c635489029fb586319a3d75dd1a0c
scotthadzik <scotthadzik@weber.edu>
2022-09-29 18:37:16 -0600

    finished string example

--- python_strings.py
+++ python_strings.py
@@ -24,38 +24,47 @@ current_year = 2022
 #       - second letter of your last name (use the -index)
 #       - first two letter of your first name (use the +index)
 #       - second two letter of your last name (use the -index)
-print (my_first_name)
-print (my_last_name)
-print(my_first_name[0])
-print(my_last_name[-5])
-print(my_first_name[0:2])
-print(my_last_name[-2:])
+# print (my_first_name)
+# print (my_last_name)
+# print(my_first_name[0])
+# print(my_last_name[-5])
+# print(my_first_name[0:2])
+# print(my_last_name[-2:])
 
 
 #TODO Combining Strings
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
 #       -first name six times
-
-
+# print(my_first_name + my_last_name)
+# print(my_first_name,my_last_name)
+# print((my_first_name +"\n") * 6)
 
 
 
 # TODO Formatting Strings
 #   - Print the following items (one per line) (print using variables)
-#       - first name last name -was born in- year of birth
+#       - Birth Year statement = first name last name -was born in- year of birth
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
-
-
+birth_year_statement = "{} {} was born in {}"
+print(birth_year_statement.format(my_first_name, my_last_name, my_birth_year))
+birth_year_celebration = "{} {} was born in {}. {} enjoyed celebrating {}"
+print(my_first_name, my_last_name, my_birth_year, my_first_name, current_year)
 
 # TODO Escape characters
 #   - Print the following items (one per line) (print using variables)
 #       - possesive first name -birth year is- year of birth 
 #       - tab last name current year
-
+escape_example = "{}\'s birth year is {}"
+print(escape_example.format(my_first_name, my_birth_year))
+escape_last_name = '\t{}'
+print(escape_last_name.format(my_last_name))
 
 # TODO String methods
 #   - Print the following items (one per line) (print using variables)
 #       - first name and last name in lower case
 #       - length of last name
 #       - first name and last name all in upper case
+print(my_first_name.casefold(), my_last_name.casefold())
+print(len(my_last_name))
+print(my_first_name.upper(), my_last_name.upper())
